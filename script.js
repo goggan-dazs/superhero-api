4b8922d1a0b9fa1937286ec4e82cb776ca5fbb12
Chris Goggans <cgogg1@gmail.com>
2022-01-12 08:05:32 -0500

    added styling

--- script.js
+++ script.js
@@ -1,3 +1,64 @@
 const key = "7d1e1bc3"
 
-console.log("hello world")
+const inputBox = document.getElementById("input");
+const searchButton = document.getElementById("button");
+let image = document.getElementById("image");
+
+searchButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let superheroName = inputBox.value;
+    console.log(superheroName)
+    
+    const url = `https://www.omdbapi.com/?s=${superheroName}&apikey=7d1e1bc3` 
+
+    
+    console.log(url)
+    
+    
+    
+    // fetch(url)
+    // .then((response) => response.json())
+    // .then(responseData => {
+    //     showResults(responseData.Search)})
+    // .catch(error => alert("Make sure to put your API key in the code"));
+    fetch(url)
+    .then((response) => response.json())
+    .then(responseData => {
+        showResults(responseData.Search)})
+    .catch(error => alert("Make sure to put your API key in the code"));
+});
+
+function showResults(results) {
+    console.log(results)
+    results.forEach(result => {
+      let item = document.createElement("li");
+      item.innerHTML = result.Title;
+      document.getElementById("results").appendChild(item);
+    });
+    
+   };
+
+//   searchButton.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     let superheroName = inputBox.value;
+//     console.log(superheroName)
+    
+//     const url = `https://www.omdbapi.com/?s=${superheroName}&apikey=7d1e1bc3` 
+    
+//     fetch(url)
+//     .then((response) => response.json())
+//     .then(responseData => {
+//         showResults(responseData.Search)})
+//     .catch(error => alert("Make sure to put your API key in the code"));
+// });
+// function showResults(results) {
+//     console.log(results)
+//     results.forEach(result => {
+//       let item = document.createElement("img");
+//       item.innerHTML = result.Poster;
+//       document.getElementById("results").appendChild(item);
+//     });
+    
+//   };
+
+
